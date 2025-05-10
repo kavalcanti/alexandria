bf1d46fdcb64fbaa0b0c877bb2ad0fdca5d3ec70
Kaoue Cavalcanti <knc@tuta.io>
2025-05-10 09:45:36 -0300

    updated start script

--- alexandria.sh
+++ alexandria.sh
@@ -2,7 +2,4 @@
 
 eval "$(conda shell.bash hook)"
 conda activate /home/kaoue/code/alexandria/.conda
-export VLLM_CONFIGURE_LOGGING=1
-export VLLM_LOGGING_LEVEL=ERROR # Optional, try if needed
-export VLLM_LOGGING_CONFIG_PATH=conf/logging_config.json
 python main.py
