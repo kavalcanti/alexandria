b9ccb5dfcc145727dad777bc2f9250d4f986536d
Kaoue Cavalcanti <knc@tuta.io>
2025-05-10 09:45:21 -0300

    removed vllm dependencies from start script

--- alexandria.sh
+++ alexandria.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# conda activate /home/kaoue/code/alexandria/.conda
+
+eval "$(conda shell.bash hook)"
+conda activate /home/kaoue/code/alexandria/.conda
 export VLLM_CONFIGURE_LOGGING=1
 export VLLM_LOGGING_LEVEL=ERROR # Optional, try if needed
 export VLLM_LOGGING_CONFIG_PATH=conf/logging_config.json
