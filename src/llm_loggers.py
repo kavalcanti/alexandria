bb2104efb300466676e2150a23dd946058eb9f88
Kaoue Cavalcanti <knc@tuta.io>
2025-05-10 00:10:32 -0300

    introduced llm loggers placeholdes. guture feature: handle db CRUD for messages.

---
+++ src/llm_loggers.py
@@ -0,0 +1,25 @@
+
+# TODO create DbMemory class
+# TODO create Chat(DbMemory) class
+# TODO create Messages(DbMemory) class
+
+
+
+class DbMemory:
+    def __init__(self):
+        # TODO initialize db_models / connections
+        return None
+
+
+class Chat(DbMemory):
+    def __init__(self):
+        ### NO RECURSIVE DEPENDENCIES
+        ### need to implement titling without calling model_calls
+        # Chat CRUD to db
+
+        return None
+
+class Messages(DbMemory):
+    def __init__(self):
+        ### Message CRUD to db
+        return None
